7881e60dbe7c822e6109b8bd78c363e70cf393d9
Bhushan Alhat <alhatbhushan@gmail.com>
2024-06-06 16:56:19 +0530

    [Frontend] feat: added video player

--- frontend/src/routes/Home.route.tsx
+++ frontend/src/routes/Home.route.tsx
@@ -1,8 +1,12 @@
+import { Link } from 'react-router-dom';
+
 function Home() {
 	return (
 		<>
 			<h1 className='text-3xl font-bold underline'>Youtube Clone</h1>
 			<h2 className='text-2xl'>Home Page</h2>
+			<Link to='/upload'>Upload</Link>
+			<Link to='/watch'>Watch</Link>
 		</>
 	);
 }
--- frontend/src/routes/Watch.route.tsx
+++ frontend/src/routes/Watch.route.tsx
@@ -1,8 +1,14 @@
+import ReactPlayer from 'react-player';
+
 function Watch() {
 	return (
 		<>
 			<h1 className='text-3xl font-bold underline'>Youtube Clone</h1>
 			<h2 className='text-2xl'>Watch Page</h2>
+			<ReactPlayer
+				url={'https://www.youtube.com/watch?v=LMYWGpt6ppM'}
+				controls
+			/>
 		</>
 	);
 }
