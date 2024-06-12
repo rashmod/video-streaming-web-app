0d83896dc7334a2ee50af206aa5428e33b255cbb
Bhushan Alhat <alhatbhushan@gmail.com>
2024-06-12 23:49:03 +0530

    [Upload-Service] feat: Implemented image upload handler

--- backend/upload_service/src/index.ts
+++ backend/upload_service/src/index.ts
@@ -1,28 +1,37 @@
 import express from 'express';
 import { PutObjectCommand, PutObjectCommandInput } from '@aws-sdk/client-s3';
-import { readFileSync } from 'fs';
+import fs from 'fs';
+import asyncFs from 'fs/promises';
 import path from 'path';
 import cors from 'cors';
+import multer from 'multer';
 
 import client from './config/s3.config';
 import env from './config/env.config';
 
 const app = express();
-app.use(cors());
 
-app.post('/upload', async (req, res) => {
-	const fileContent = readFileSync(path.join(__dirname, 'test.png'));
+const upload = multer({ dest: './uploads/' });
+
+app.use(cors());
+app.use(express.json());
+
+app.post('/upload', upload.single('video'), async (req, res) => {
+	if (!req.file) return res.status(400).json({ message: 'No file uploaded' });
+
+	const filePath = path.join(__dirname, '..', 'uploads', req.file.filename);
+	const fileContent = fs.readFileSync(filePath);
 
 	const params: PutObjectCommandInput = {
 		Bucket: env.AWS_BUCKET_NAME,
-		Key: 'my_test_file.png',
+		Key: req.file.filename,
 		Body: fileContent,
 	};
 
 	const command = new PutObjectCommand(params);
 	await client.send(command);
-
-	console.log('uploading file...');
+	await asyncFs.access(filePath);
+	await asyncFs.unlink(filePath);
 
 	res.status(200).json({ message: 'Hello World!' });
 });
