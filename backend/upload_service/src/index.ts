b45967ff72a3bfc73c1a98dda878a67791f83897
Bhushan Alhat <alhatbhushan@gmail.com>
2024-06-20 23:11:08 +0530

    [Upload-Service] refactor: Changed base url for routes

--- backend/upload_service/src/index.ts
+++ backend/upload_service/src/index.ts
@@ -9,7 +9,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use('/upload', uploadRoutes);
+app.use('/', uploadRoutes);
 
 app.listen(envConfig.PORT, () => {
 	console.log(
