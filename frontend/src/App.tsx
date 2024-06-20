87fe256df412b36c3505cb1e63ae82835bbc3893
Bhushan Alhat <alhatbhushan@gmail.com>
2024-06-20 23:23:47 +0530

    [Frontend] refactor: Moved links to app component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Outlet } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 const queryClient = new QueryClient();
@@ -8,6 +8,8 @@ function App() {
 		<main className='min-h-screen'>
 			<QueryClientProvider client={queryClient}>
 				<Outlet />
+				<Link to='/upload'>Upload</Link>
+				<Link to='/watch'>Watch</Link>
 			</QueryClientProvider>
 		</main>
 	);
--- frontend/src/routes/Home.route.tsx
+++ frontend/src/routes/Home.route.tsx
@@ -1,12 +1,8 @@
-import { Link } from 'react-router-dom';
-
 function Home() {
 	return (
 		<>
 			<h1 className='text-3xl font-bold underline'>Youtube Clone</h1>
 			<h2 className='text-2xl'>Home Page</h2>
-			<Link to='/upload'>Upload</Link>
-			<Link to='/watch'>Watch</Link>
 		</>
 	);
 }
