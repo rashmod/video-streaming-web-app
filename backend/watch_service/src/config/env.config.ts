e07e4c1f976c38bd5d999afda8735dd286dfe141
Bhushan Alhat <alhatbhushan@gmail.com>
2024-06-21 19:49:48 +0530

    [Watch-Service] feat: initialized watch service

---
+++ backend/watch_service/src/config/env.config.ts
@@ -0,0 +1,28 @@
+import 'dotenv/config';
+import { z } from 'zod';
+
+// todo kafka topic in env
+
+const schema = z.object({
+	HOST: z.string().trim().min(1),
+	PORT: z.coerce.number().int().positive(),
+	NODE_ENV: z.enum(['DEVELOPMENT', 'PRODUCTION', 'TEST']),
+	AWS_ACCESS_KEY: z.string(),
+	AWS_SECRET_ACCESS_KEY: z.string(),
+	AWS_REGION: z.string(),
+	AWS_BUCKET_NAME: z.string(),
+
+	clientId: z.string().trim().min(1),
+});
+
+const parsedEnv = schema.safeParse(process.env);
+
+if (!parsedEnv.success) {
+	console.error(
+		'❌ Invalid environment variables:',
+		JSON.stringify(parsedEnv.error.format(), null, 4)
+	);
+	process.exit(1);
+}
+
+export default parsedEnv.data;
---
+++ backend/watch_service/src/index.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+
+import envConfig from './config/env.config';
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.use('/', (req, res) => {
+	res.send('Hello World!');
+});
+
+app.listen(envConfig.PORT, () => {
+	console.log(
+		`Server running in ${envConfig.NODE_ENV} mode on host ${envConfig.HOST} on port ${envConfig.PORT}`
+	);
+});
