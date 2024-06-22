c183db02529f1a2464a2abf5128a478a395a9285
Bhushan Alhat <alhatbhushan@gmail.com>
2024-06-22 22:21:36 +0530

    [Frontend] feat: Implemented video playback using aws signed url

---
+++ frontend/src/api/watch.ts
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+import envConfig from '../config/env.config';
+
+const WATCH_SERVICE_API_URL = envConfig.VITE_WATCH_SERVICE_API_URL;
+
+async function watchVideo(videoId: string) {
+	const response = await axios.get(
+		`${WATCH_SERVICE_API_URL}/video/${videoId}`
+	);
+	console.log(response);
+	return response.data;
+}
+
+export default { watchVideo };
--- frontend/src/pages/Watch.page.tsx
+++ frontend/src/pages/Watch.page.tsx
@@ -1,14 +1,29 @@
 import ReactPlayer from 'react-player';
+import { useQuery } from 'react-query';
+
+import watch from '../api/watch';
 
 function Watch() {
+	const videoId = '123';
+
+	const { data, isError, isLoading } = useQuery({
+		queryKey: ['video', videoId],
+		queryFn: () => watch.watchVideo(videoId),
+
+		refetchInterval: false,
+		refetchIntervalInBackground: false,
+		refetchOnMount: false,
+		refetchOnReconnect: false,
+		refetchOnWindowFocus: false,
+	});
+
 	return (
 		<>
 			<h1 className='text-3xl font-bold underline'>Youtube Clone</h1>
 			<h2 className='text-2xl'>Watch Page</h2>
-			<ReactPlayer
-				url={'https://www.youtube.com/watch?v=LMYWGpt6ppM'}
-				controls
-			/>
+			{isLoading && <p>Loading...</p>}
+			{isError && <p>Error</p>}
+			{!isLoading && !isError && <ReactPlayer url={data.url} controls />}
 		</>
 	);
 }
