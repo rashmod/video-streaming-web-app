24a23de5f248b094e577a85c73739133692f7607
Bhushan Alhat <alhatbhushan@gmail.com>
2024-06-27 22:17:23 +0530

    [Transcode-Service] feat: Implemented video transcoding to multiple resolutions

--- backend/transcode_service/src/controller/transcode.controller.ts
+++ backend/transcode_service/src/controller/transcode.controller.ts
@@ -1,4 +1,4 @@
-import ffmpeg, { FfprobeData } from 'fluent-ffmpeg';
+import ffmpeg, { type FfprobeData } from 'fluent-ffmpeg';
 import { promisify } from 'util';
 
 import downloadInChunks from '../services/downloadInChunks';
@@ -21,7 +21,7 @@ export default async function transcodeController(str: string | undefined) {
 	// todo get videoName from db
 	const videoName = videoId;
 
-	const filePath = generateFilePath(videoName);
+	const filePath = generateFilePath(videoName, 'input');
 
 	await downloadInChunks(bucket, videoId, filePath);
 	console.log('downloaded in chunks...');
@@ -43,4 +43,37 @@ export default async function transcodeController(str: string | undefined) {
 	};
 
 	console.log('Resolution:', resolution);
+
+	const VARIANTS = [
+		{ width: 1920, height: 1080, name: '1080p' },
+		{ width: 1280, height: 720, name: '720p' },
+		{ width: 854, height: 480, name: '480p' },
+		// { width: 640, height: 360, name: '360p' },
+		// { width: 426, height: 240, name: '240p' },
+		// { width: 320, height: 180, name: '180p' },
+	];
+
+	VARIANTS.forEach((variant) => {
+		const outputFileName = `output-${variant.name}.m3u8`;
+		const outputFilePath = generateFilePath(outputFileName, 'output');
+
+		ffmpeg(filePath)
+			.outputOptions([
+				'-profile:v baseline',
+				'-level 3.0',
+				`-s ${variant.width}x${variant.height}`,
+				'-start_number 0',
+				'-hls_time 10',
+				'-hls_list_size 0',
+				'-f hls',
+			])
+			.output(outputFilePath)
+			.on('end', () => {
+				console.log(`Conversion completed for ${outputFileName}`);
+			})
+			.on('error', (err) => {
+				console.log('Error during transcoding:', err);
+			})
+			.run();
+	});
 }
--- backend/transcode_service/src/utilities/generateFilePath.ts
+++ backend/transcode_service/src/utilities/generateFilePath.ts
@@ -1,8 +1,11 @@
 import fs from 'fs';
 import path from 'path';
 
-export default function generateFilePath(videoName: string) {
-	const inputDir = './input/';
+export default function generateFilePath(
+	videoName: string,
+	dir: 'input' | 'output'
+) {
+	const inputDir = `./${dir}/`;
 	if (!fs.existsSync(inputDir)) {
 		fs.mkdirSync(inputDir);
 	}
