e828a30d4329f1b4e22f1d66a0f8e41d28c7261e
Bhushan Alhat <alhatbhushan@gmail.com>
2024-06-28 19:54:47 +0530

    [Transcode-Service] refactor: Promisified ffmpeg video transcoding

--- backend/transcode_service/src/utilities/transcodeVideo.ts
+++ backend/transcode_service/src/utilities/transcodeVideo.ts
@@ -1,32 +1,47 @@
 import Ffmpeg from 'fluent-ffmpeg';
 
-export default function transcodeVideo(
-	inputFilePath: string,
-	outputFileName: string,
-	outputFilePath: string,
+export default function transcodeVideo({
+	inputFilePath,
+	outputFileName,
+	outputFilePath,
+	variant,
+	hlsTime = 10,
+}: {
+	inputFilePath: string;
+	outputFileName: string;
+	outputFilePath: string;
 	variant: {
 		width: number;
 		height: number;
 		name: string;
-	},
-	hlsTime = 10
-) {
-	Ffmpeg(inputFilePath)
-		.outputOptions([
-			'-profile:v baseline',
-			'-level 3.0',
-			`-s ${variant.width}x${variant.height}`,
-			'-start_number 0',
-			`-hls_time ${hlsTime}`,
-			'-hls_list_size 0',
-			'-f hls',
-		])
-		.output(outputFilePath)
-		.on('end', () => {
-			console.log(`Conversion completed for ${outputFileName}`);
-		})
-		.on('error', (err) => {
-			console.log('Error during transcoding:', err);
-		})
-		.run();
+	};
+	hlsTime?: number;
+}) {
+	return new Promise((resolve, reject) => {
+		Ffmpeg(inputFilePath)
+			.outputOptions([
+				'-profile:v baseline',
+				'-level 3.0',
+				`-s ${variant.width}x${variant.height}`,
+				'-start_number 0',
+				`-hls_time ${hlsTime}`,
+				'-hls_list_size 0',
+				'-f hls',
+			])
+			.output(outputFilePath)
+			.on('start', () => {
+				console.log(`Started transcoding ${outputFileName}`);
+				console.log(`resolution: ${variant.name}`);
+			})
+			.on('end', () => {
+				console.log(`Conversion completed for ${outputFileName}`);
+				console.log(`resolution: ${variant.name}`);
+				resolve(true);
+			})
+			.on('error', (err) => {
+				console.log('Error during transcoding:', err);
+				reject(false);
+			})
+			.run();
+	});
 }
