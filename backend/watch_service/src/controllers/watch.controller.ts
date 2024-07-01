82698e9f0f214d8b56b0a301e6344ea94ead3093
Bhushan Alhat <alhatbhushan@gmail.com>
2024-07-01 19:10:19 +0530

    [Watch-Service] feat: Generate signed url for cloudfront distribution

--- backend/watch_service/src/controllers/watch.controller.ts
+++ backend/watch_service/src/controllers/watch.controller.ts
@@ -1,8 +1,6 @@
 import { Request, Response } from 'express';
-import { GetObjectCommand } from '@aws-sdk/client-s3';
-import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+import { getSignedUrl } from '@aws-sdk/cloudfront-signer';
 
-import s3Client from '../config/s3.config';
 import envConfig from '../config/env.config';
 import { WEEK } from '../constants/constants';
 
@@ -13,16 +11,38 @@ export default async function watchController(
 	const { videoId } = req.params;
 
 	// get s3 image name from video id
-
 	const videoName =
-		'1718964827386f9b69cbb38b980af49400fb93d3fbbb61f5dc1c874379324f3df98e93f96af93.mp4';
+		'1719739627413f7c8788209e850a52f9d984a623b0c364f1ff89f1e89380a5acdf79d2cb2347a_mp4';
 
-	const command = new GetObjectCommand({
-		Bucket: envConfig.AWS_BUCKET_NAME,
-		Key: videoName,
+	const policy = {
+		Statement: [
+			{
+				Resource: `${envConfig.AWS_CLOUDFRONT_URL}/${videoName}/*`,
+				Condition: {
+					DateLessThan: {
+						'AWS:EpochTime': Math.floor(
+							(new Date().getTime() + WEEK) / 1000
+						),
+					},
+				},
+			},
+		],
+	};
+
+	const customPolicy = JSON.stringify(policy);
+
+	const signedUrl = getSignedUrl({
+		policy: customPolicy,
+		keyPairId: envConfig.AWS_CLOUDFRONT_KEY_PAIR_ID,
+		privateKey: envConfig.AWS_CLOUDFRONT_PRIVATE_KEY,
 	});
 
-	const url = await getSignedUrl(s3Client, command, { expiresIn: WEEK });
+	const token = signedUrl.split('?')[1];
+	const url = `${envConfig.AWS_CLOUDFRONT_URL}/${videoName}/${videoName}.m3u8`;
 
-	res.status(200).json({ message: 'Successfully generated signed url', url });
+	res.status(200).json({
+		message: 'Successfully generated signed url',
+		token: '?' + token,
+		url,
+	});
 }
