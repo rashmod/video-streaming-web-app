c0b09508d85fb76896ae9773bed9d17a49154888
Bhushan Alhat <alhatbhushan@gmail.com>
2024-07-05 22:25:10 +0530

    [Frontend] style: Added styling to navbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import { QueryClient, QueryClientProvider } from 'react-query';
 
 import Navbar from './components/custom/Navbar';
@@ -14,11 +14,6 @@ function App() {
 						<Outlet />
 					</Navbar>
 				</main>
-				<div className='sticky bottom-0 left-0 z-10 flex items-center justify-around w-full py-3 bg-white shadow-t dark:bg-gray-950'>
-					<Link to='/'>Home</Link>
-					<Link to='/upload'>Upload</Link>
-					<Link to='/watch'>Watch</Link>
-				</div>
 			</QueryClientProvider>
 		</main>
 	);
--- frontend/src/components/custom/Navbar.tsx
+++ frontend/src/components/custom/Navbar.tsx
@@ -1,13 +1,14 @@
 import { Link } from 'react-router-dom';
-import { HomeIcon, MonitorPause, SquareUser } from 'lucide-react';
+import { CloudUpload, HomeIcon, LogIn, MonitorPause } from 'lucide-react';
 
 import { Button } from '@/components/ui/button';
-import SearchBox from './SearchBox';
+import SearchBox from '@/components/custom/SearchBox';
+import BottomNavigationLink from '@/components/custom/BottomNavigationLink';
 
 export default function Navbar({ children }: { children: React.ReactNode }) {
 	return (
 		<>
-			<header className='sticky top-0 z-10 flex items-center justify-between w-full px-4 py-3 mb-5 bg-white shadow-sm text-gray-950 dark:text-white dark:bg-gray-950 sm:px-6 md:px-8 lg:px-10'>
+			<header className='sticky top-0 z-10 flex items-center justify-between w-full px-4 py-3 mb-5 bg-white text-gray-950 dark:text-white dark:bg-gray-950 sm:px-6 md:px-8 lg:px-10'>
 				<Link to='#' className='flex items-center gap-2'>
 					<MonitorPause className='w-6 h-6' />
 					<span className='text-xl font-semibold'>Streamline</span>
@@ -16,28 +17,57 @@ export default function Navbar({ children }: { children: React.ReactNode }) {
 				<SearchBox />
 
 				<nav className='items-center hidden gap-6 text-sm font-medium lg:flex'>
-					<Link to='#'>
-						<Button variant='link'>Home</Button>
+					<Link to='/'>
+						<Button size='sm' variant='ghost'>
+							Home
+						</Button>
 					</Link>
-					<Link to='#'>
-						<Button size='sm'>Sign In</Button>
+					{/* <Link to='#'>
+						<Button size='sm' variant='ghost'>
+							Profile
+						</Button>
+					</Link> */}
+					<Link to='upload'>
+						<Button size='sm' variant='ghost'>
+							Upload
+						</Button>
+					</Link>
+					{/* <Link to='#'>
+						<Button size='sm' variant='ghost'>
+							Log Out
+						</Button>
+					</Link> */}
+					{/* <Link to='register'>
+						<Button
+							size='sm'
+							variant='outline'
+							className='border-2'>
+							Register
+						</Button>
+					</Link> */}
+					<Link to='log-in'>
+						<Button size='sm' variant='default'>
+							Log In
+						</Button>
 					</Link>
 				</nav>
 			</header>
 			{children}
+			<div className='invisible mt-0 lg:mt-5' />
 			<header className='sticky bottom-0 left-0 z-10 flex items-center justify-around w-full py-3 mt-5 bg-white shadow-t dark:bg-gray-950 lg:hidden'>
-				<Link
+				<BottomNavigationLink to='/' title='Home' Icon={HomeIcon} />
+				{/* <BottomNavigationLink
 					to='#'
-					className='flex flex-col items-center gap-1 text-xs font-medium text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50'>
-					<HomeIcon className='w-6 h-6' />
-					Home
-				</Link>
-				<Link
-					to='#'
-					className='flex flex-col items-center gap-1 text-xs font-medium text-gray-500 hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50'>
-					<SquareUser className='w-6 h-6' />
-					Profile
-				</Link>
+					title='Profile'
+					Icon={SquareUser}
+				/> */}
+				<BottomNavigationLink
+					to='upload'
+					title='Upload'
+					Icon={CloudUpload}
+				/>
+				{/* <BottomNavigationLink to='#' title='Log Out' Icon={LogOut} /> */}
+				<BottomNavigationLink to='log-in' title='Log In' Icon={LogIn} />
 			</header>
 		</>
 	);
