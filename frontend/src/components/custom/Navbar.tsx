1edcf9ad4b22d927b638722558a8a39965429197
Bhushan Alhat <alhatbhushan@gmail.com>
2024-07-07 16:16:52 +0530

    [Frontend] feat: Improved navbar responsiveness and search bar visibility for small screens

--- frontend/src/components/custom/Navbar.tsx
+++ frontend/src/components/custom/Navbar.tsx
@@ -1,20 +1,33 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { CloudUpload, HomeIcon, LogIn, MonitorPause } from "lucide-react";
+
+import { cn } from "@/lib/utils";
 
 import { Button } from "@/components/ui/button";
 import SearchBox from "@/components/custom/SearchBox";
 import BottomNavigationLink from "@/components/custom/BottomNavigationLink";
 
 export default function Navbar({ children }: { children: React.ReactNode }) {
+  const [isSearchOpen, setIsSearchOpen] = useState(false);
+
   return (
     <>
-      <header className="sticky top-0 z-10 mb-5 flex w-full items-center justify-between bg-white px-4 py-3 text-gray-950 dark:bg-gray-950 dark:text-white sm:px-6 md:px-8 lg:px-10">
+      <header
+        className={cn(
+          "sticky top-0 z-10 mb-5 flex w-full items-center justify-between bg-white bg-opacity-90 px-4 py-3 text-gray-950 shadow-[0_24px_24px_-6px_rgba(0,0,0,0.1)] backdrop-blur-sm dark:bg-gray-950 dark:text-white sm:px-6 md:px-8 lg:px-10",
+          { "shadow-none": isSearchOpen },
+        )}
+      >
         <Link to="#" className="flex items-center gap-2">
           <MonitorPause className="h-6 w-6" />
           <span className="text-xl font-semibold">Streamline</span>
         </Link>
 
-        <SearchBox />
+        <SearchBox
+          isSearchOpen={isSearchOpen}
+          setIsSearchOpen={setIsSearchOpen}
+        />
 
         <nav className="hidden items-center gap-6 text-sm font-medium lg:flex">
           <Link to="/">
--- frontend/src/components/custom/SearchBox.tsx
+++ frontend/src/components/custom/SearchBox.tsx
@@ -1,25 +1,49 @@
-import { SearchIcon } from "lucide-react";
+import { SearchIcon, XIcon } from "lucide-react";
 
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
+import { cn } from "@/lib/utils";
 
-export default function SearchBox() {
+export default function SearchBox({
+  isSearchOpen,
+  setIsSearchOpen,
+}: {
+  isSearchOpen: boolean;
+  setIsSearchOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) {
   return (
-    <div className="flex items-center gap-4">
-      <form className="relative w-full lg:block lg:w-auto">
-        <div className="absolute inset-y-0 left-0 flex items-center pl-3">
+    <>
+      <Button
+        variant="ghost"
+        size="icon"
+        className="rounded-full lg:hidden"
+        onClick={() => setIsSearchOpen(!isSearchOpen)}
+      >
+        {isSearchOpen ? (
+          <XIcon className="h-5 w-5" />
+        ) : (
+          <SearchIcon className="h-5 w-5" />
+        )}
+        <span className="sr-only">Search</span>
+      </Button>
+
+      <form
+        className={cn(
+          "absolute left-0 top-full z-20 hidden shadow-[0_24px_24px_-6px_rgba(0,0,0,0.1)] lg:relative lg:block lg:shadow-none",
+          {
+            "block w-full": isSearchOpen,
+          },
+        )}
+      >
+        <div className="absolute inset-y-0 left-0 hidden items-center pl-3 lg:flex">
           <SearchIcon className="h-5 w-5 text-gray-400" />
         </div>
         <Input
           type="search"
           placeholder="Search..."
-          className="h-9 w-full rounded-md border border-gray-300 bg-gray-100 pl-10 text-sm focus:border-gray-500 focus:bg-white focus:outline-none dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200 dark:focus:border-gray-600 lg:w-[600px]"
+          className="h-9 w-full rounded-md border border-gray-300 bg-gray-100 text-sm focus:border-gray-500 focus:bg-white focus:outline-none dark:border-gray-700 dark:bg-gray-800 dark:text-gray-200 dark:focus:border-gray-600 lg:w-[600px] lg:pl-10"
         />
       </form>
-      <Button variant="ghost" size="icon" className="rounded-full lg:hidden">
-        <SearchIcon className="h-5 w-5" />
-        <span className="sr-only">Search</span>
-      </Button>
-    </div>
+    </>
   );
 }
