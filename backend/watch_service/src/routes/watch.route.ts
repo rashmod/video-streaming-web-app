b3d457081a38cf1ea951fdcd1699186e59fdc60d
Bhushan Alhat <alhatbhushan@gmail.com>
2024-07-11 00:42:32 +0530

    [Watch-Service] chore: Deleted watch service

--- backend/watch_service/src/routes/watch.route.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-import watchController from '../controllers/watch.controller';
-
-const router = express.Router();
-
-router.get('/video/:videoId', watchController);
-
-export default router;
