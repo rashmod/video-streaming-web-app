ac210172655c4234015b55043bbbd037b81f7974
Bhushan Alhat <alhatbhushan@gmail.com>
2024-07-17 23:54:12 +0530

    [Transcode-Service] feat: Use video name and upload key from database for file handling

--- backend/transcode_service/src/processor/transcode.processor.ts
+++ backend/transcode_service/src/processor/transcode.processor.ts
@@ -2,39 +2,51 @@ import TranscodeService from '../services/transcode.service';
 import DownloadService from '../services/download.service';
 import S3Service from '../services/s3.service';
 import FileService from '../services/file.service';
+import VideoService from '../services/video.service';
 
 import { TRANSCODE_DIRECTORY, VARIANTS } from '../constants/constants';
+import UploadProgressService from '../services/uploadProgress.service';
 
 export default async function transcodeProcessor(
 	videoId: string,
 	resumeConsumer: () => void
 ) {
+	const { videoName: transcodeKey } = await VideoService.findById(videoId);
+	const { uploadKey } = await UploadProgressService.findById(videoId);
 
+	const transcodeKeyBase = FileService.getS3FileName({
+		fileName: transcodeKey,
+		type: 'base',
+	});
+	const uploadKeyBase = FileService.getS3FileName({
+		fileName: uploadKey,
+		type: 'base',
+	});
 
 	const downloadedFilePath = await FileService.generateDownloadFilePath(
-		videoName
+		uploadKeyBase
 	);
 
 	await DownloadService.downloadFile({
-		videoName: videoId,
+		videoName: uploadKey,
 		destinationPath: downloadedFilePath,
 	});
 
 	await TranscodeService.transcodeVariants({
-		videoName,
+		videoName: transcodeKeyBase,
 		inputFilePath: downloadedFilePath,
 		variants: VARIANTS,
 	});
 
-	const videos = await FileService.getFilesPath(TRANSCODE_DIRECTORY);
+	const videos = await FileService.getFilesPath(
+		TRANSCODE_DIRECTORY,
+		transcodeKeyBase
+	);
 
-	// todo save the transcoded files with a prefix video
-	// todo save the original files with a prefix original
-	// todo save the thumbnails with a prefix thumbnail
 	await S3Service.uploadFiles(videos);
 
 	const transcodedVideoDirectory =
-		FileService.getTranscodedVideoDirectory(videoName);
+		FileService.getTranscodedVideoDirectory(transcodeKeyBase);
 	await FileService.deleteDirectory(transcodedVideoDirectory);
 	await FileService.deleteFile(downloadedFilePath);
 
