02f4713e9a71250a83361876e99556eed65e8ca6
Bhushan Alhat <alhatbhushan@gmail.com>
2024-07-18 17:03:05 +0530

    [Frontend] refactor: Add function overloading to ensure correct return types in process file utility

--- frontend/src/utilities/processFiles.ts
+++ frontend/src/utilities/processFiles.ts
@@ -1,11 +1,20 @@
 import { FileWithUrl } from "@/types/types";
 
-export default function processFiles(
+function processFiles(fileList: FileList): FileWithUrl;
+function processFiles(
+  fileList: FileList,
+  MAX_FILE_SIZE: number,
+): FileWithUrl | undefined;
+
+function processFiles(
   fileList: FileList,
   MAX_FILE_SIZE?: number,
 ): FileWithUrl | undefined {
-  const file = fileList[0];
-  if (MAX_FILE_SIZE === undefined || file.size <= MAX_FILE_SIZE) {
+  const [file] = fileList;
+
+  if (file && (MAX_FILE_SIZE === undefined || file.size <= MAX_FILE_SIZE)) {
     return { file, localURL: URL.createObjectURL(file) };
   }
 }
+
+export default processFiles;
