d3e6986732ef679eeb4fd859088d86146ccd523f
Bhushan Alhat <alhatbhushan@gmail.com>
2024-07-18 16:48:24 +0530

    [Frontend] refactor: Update api function to handle revised api responses for uploading

--- frontend/src/api/upload.ts
+++ frontend/src/api/upload.ts
@@ -1,52 +1,41 @@
 import axios from "axios";
 
-import envConfig from "../config/env.config";
+import envConfig from "@/config/env.config";
+import { InitializeUploadResponse } from "@/types/types";
 
 const UPLOAD_SERVICE_API_URL = envConfig.VITE_UPLOAD_SERVICE_API_URL;
 
-async function initializeUpload({
-  title,
-  thumbnail,
-  extension,
-}: {
-  title: string;
-  thumbnail: File;
-  extension: string;
-}): Promise<{
-  uploadId: string;
-  videoId: string;
-}> {
-  const response = await axios.post(`${UPLOAD_SERVICE_API_URL}/initialize`, {
-    title,
-    thumbnail,
-    extension,
-  });
+async function initializeUpload(
+  initializeData: FormData,
+): Promise<InitializeUploadResponse> {
+  const response = await axios.post(
+    `${UPLOAD_SERVICE_API_URL}/initialize`,
+    initializeData,
+    { headers: { "Content-Type": "multipart/form-data" } },
+  );
   console.log(response);
-  return response.data;
+  return response.data.data;
 }
 
-async function uploadVideo(
-  uploadData: FormData,
-): Promise<{ message: string; eTag: string | undefined }> {
+async function uploadVideo(uploadData: FormData): Promise<string> {
   const response = await axios.post(
     `${UPLOAD_SERVICE_API_URL}/upload`,
     uploadData,
-    {
-      headers: { "Content-Type": "multipart/form-data" },
-    },
+    { headers: { "Content-Type": "multipart/form-data" } },
   );
   console.log(response);
-  return response.data;
+  return response.data.data;
 }
 
 async function completeUpload(data: {
+  resolution: { height: number; width: number };
   uploadId: string;
   videoId: string;
   parts: { ETag: string | undefined; PartNumber: number }[];
 }): Promise<{ message: string }> {
   const response = await axios.post(`${UPLOAD_SERVICE_API_URL}/complete`, data);
   console.log(response);
-  return response.data;
+  return response.data.data;
 }
 
 export default { initializeUpload, uploadVideo, completeUpload };
--- frontend/src/types/types.ts
+++ frontend/src/types/types.ts
@@ -2,3 +2,39 @@ export type FileWithUrl = {
   file: File;
   localURL: string;
 };
+
+export type InitializeUploadResponse = {
+  video: Video;
+  videoState: VideoState;
+  uploadProgress: UploadProgress;
+};
+
+export type UploadProgress = {
+  id: string;
+  uploadId: string;
+  uploadKey: string;
+  totalParts: number;
+  uploadedParts: number;
+  createdAt: Date;
+  updatedAt: Date;
+  videoId: string;
+};
+
+export type Video = {
+  id: string;
+  title: string;
+  duration: number;
+  thumbnailName: string;
+  videoName: string;
+  createdAt: Date;
+  updatedAt: Date;
+  userId: string;
+};
+
+export type VideoState = {
+  id: string;
+  status: string;
+  createdAt: Date;
+  updatedAt: Date;
+  videoId: string;
+};
