2f60dcf96a4b08b44994f556ae05d4f1b83c2c54
Bhushan Alhat <alhatbhushan@gmail.com>
2024-08-01 22:25:34 +0530

    [User-Service] feat: add authentication middleware for secure routes

--- backend/user_service/src/routes/user.route.ts
+++ backend/user_service/src/routes/user.route.ts
@@ -2,14 +2,17 @@ import express from 'express';
 
 import { UserController } from '../controllers';
 import catchAsync from '../utilities/catchAsync';
+import authMiddleware from '../middlewares/auth.middleware';
 
 const router = express.Router();
 
-router.post('/register', catchAsync(UserController.registerUser));
-router.post('/login', catchAsync(UserController.loginUser));
-router.post('/logout', UserController.logoutUser);
-router.get('/:id', catchAsync(UserController.getUser));
-router.put('/:id', catchAsync(UserController.updateUser));
-router.delete('/:id', catchAsync(UserController.deleteUser));
+router.route('/register').post(catchAsync(UserController.registerUser));
+router.route('/login').post(catchAsync(UserController.loginUser));
+router.route('/logout').post(UserController.logoutUser);
+router
+	.route('/:id')
+	.get(catchAsync(UserController.getUser))
+	.put(authMiddleware, catchAsync(UserController.updateUser))
+	.delete(authMiddleware, catchAsync(UserController.deleteUser));
 
 export default router;
