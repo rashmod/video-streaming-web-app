5463eb76510042bc5fc3dbc88845ac33f53056e1
Bhushan Alhat <alhatbhushan@gmail.com>
2024-08-05 13:28:28 +0530

    [Video-Service] refactor: updated async error handler to use specific types

--- backend/video_service/src/utilities/catchAsync.ts
+++ backend/video_service/src/utilities/catchAsync.ts
@@ -1,5 +1,7 @@
+import { Request, Response, NextFunction } from 'express';
+
 export default function catchAsync(fn: Function) {
-	return (req: any, res: any, next: any) => {
+	return (req: Request, res: Response, next: NextFunction) => {
 		fn(req, res, next).catch((err: any) => next(err));
 	};
 }
